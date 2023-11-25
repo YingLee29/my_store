eb49bf7db0ba6b1d5b7029c96563306034880437
YingLee29 <lequan13576@gmail.com>
2023-11-25 10:55:15 +0700

    setting js

---
+++ app/javascript/controllers/hello_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  connect() {
+    // $(function () {
+    //   setTimeout(function () {
+    //     $('.hidden-flash').addClass('d-none');
+    //   }, 3000);
+    // });
+  }
+}
